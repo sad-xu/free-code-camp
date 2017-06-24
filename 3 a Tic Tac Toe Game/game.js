7a06254cea7c5c297010cb7dec09b21f38fd046d
xhc <1031568754@qq.com>
2017-06-24 12:33:16 +0800

    完善游戏结束处理

--- 3 a Tic Tac Toe Game/game.js
+++ 3 a Tic Tac Toe Game/game.js
@@ -2,12 +2,15 @@
 var board = document.getElementsByClassName('chessboard')[0];
 var startWrapper = document.getElementsByClassName('start-wrapper')[0];
 var pauseWrapper = document.getElementsByClassName('pause-wrapper')[0];
-var you = "";  //玩家执什么子    O 0   x 1
-var ai = "";   //ai执子
-var order = 0; //ai先手:0  后手:1  默认先手
+var end = document.getElementsByClassName('end')[0];
+var restart = document.getElementsByName('restart')[0];
+
+var you = "";  // 玩家执子    O 0   x 1
+var ai = "";   // ai执子
+var order = 0; // ai先手:0  后手:1  默认先手
 var Arr = [];
-var step = 0; //	步数
-var winArr = [   //获胜可能
+var step = 0; // 步数
+var winArr = [   // 获胜可能
 							[[0, 0], [0, 1], [0, 2]],
 							[[1, 0], [1, 1], [1, 2]],
 							[[2, 0], [2, 1], [2, 2]],
@@ -17,7 +20,7 @@ var winArr = [   //获胜可能
 							[[0, 0], [1, 1], [2, 2]],
 							[[0, 2], [1, 1], [2, 0]]
 						];
-//	开始前,选择执子,先手后手
+// 开始前,选择执子,先手后手
 startWrapper.addEventListener("click", function(event) {
 	var target = event.target;
 	var round = document.getElementsByName('round')[0];
@@ -63,17 +66,17 @@ function gameStart() {
 	AI();
 }
 
-//	开始
+// 开始
 function Cell(loc, ele, val) {
-	this.loc = loc;		//位置[0,0]
-	this.ele = ele;		//元素
-	this.val = val;		//落子 null fork  round 
+	this.loc = loc;		// 位置[0,0]
+	this.ele = ele;		// 元素
+	this.val = val;		// 落子 null fork  round 
 }
 
-function start() {    //	初始化Arr[]
+function start() {    // 初始化Arr[]
 	var eleArr = [];
 	board.firstElementChild.childNodes.forEach(function(item,index,array) {
-		if (item.nodeType === 1) { //去除空白节点
+		if (item.nodeType === 1) { // 去除空白节点
 			item.childNodes.forEach(function(ite, ind, arr) {
 				if (ite.nodeType === 1) {
 					eleArr.push(ite);
@@ -89,26 +92,36 @@ function start() {    //	初始化Arr[]
 }
 
 function dropChess(i, j) {   // 落子
-	pauseWrapper.setAttribute("style", "display:block");  //遮罩开
+	pauseWrapper.setAttribute("style", "display:block");  // 遮罩开
 	setTimeout(function() {
 		Arr[i * 3 + j].val = ai;
 		Arr[i * 3 + j].ele.setAttribute("class", ai);	
 		step++;
 		console.log(step)
-		pauseWrapper.setAttribute("style", "display:none");   //遮罩关
+		pauseWrapper.setAttribute("style", "display:none");   // 遮罩关
+
+		win = winner("ai");  
+		console.log('win = ' + win);
+	  if (win === 3) {
+			gameOver("You loss");   
+			return;
+		} else if (win === 9) {
+			gameOver("平局");        // 最后一子是ai下的
+			return;
+		}			
 	}, 1000);
 }
 
 function AI() {		// AI
 	// 第一步
 	if (order !== -1) {  
-		if (order === 0) { //先手 下中间
+		if (order === 0) { // 先手 下中间
 			dropChess(1, 1);
 			order = -1;
 		} else if (order === 1 && step === 1) { //后手
 			if (!Arr[4].val) { // 优先下中间
 				dropChess(1, 1);
-			} else {		//其次下角位
+			} else {		// 其次下角位
 				dropChess((Math.random() > 0.5) ? 2 : 0, (Math.random() > 0.5) ? 2 : 0);
 			}
 			order = -1;
@@ -116,18 +129,22 @@ function AI() {		// AI
 		return;
 	}
 
-	// 攻守
-	yous = scan()[0];
-	ais = scan()[1];
+	var win = winner("you");
+	if (win === 9) {
+		gameOver("平局");      // 最后一子是玩家下的
+		return;
+	}
 
-	
+	// 攻守
+	var yous = scan()[0];
+	var ais = scan()[1];
 	if (attDef(ais).length > 0) {			// 攻
 		var target = attDef(ais);
 		dropChess(target[0][0], target[0][1]);
 	} else if (attDef(yous).length > 0) { 		// 守
 		var target = attDef(yous);
 		dropChess(target[0][0], target[0][1]);
-	} else {		//	垃圾时间
+	} else {		// 垃圾时间
 		var empty = [];
 		Arr.forEach(function(item) {
 			if (!item.val) {
@@ -139,49 +156,9 @@ function AI() {		// AI
 			dropChess(empty[a].loc[0], empty[a].loc[1]); 
 		}
 	}
-
-
-	//检测胜负
-	yous = scan()[0];
-	ais = scan()[1];
-	whoWin("AI", ais);
-	console.log(yous.length + ais.length)
-	if (yous.length + ais.length === 9) {
-		alert("平局");
-	}		
-
 }
 
-function whoWin(who, arr){
-	var flag = 0;
-	winArr.forEach(function(item) {
-		flag = 0;
-		item.forEach(function(ite) {
-			arr.forEach(function(ite2) {
-				if (ite2.loc.toString() === ite.toString()) {
-					flag++;
-				}
-			});					
-			if (flag === 3) {
-				alert( who + " Win");
-			}
-		});	
-	});	
-}
-
-function scan(yous, ais){  // 扫描棋盘
-	var yous = [], ais = [];
-	Arr.forEach(function(item, index, array) {
-		if (item.val == you) {
-			yous.push(item);
-		} else if (item.val == ai) {
-			ais.push(item);
-		}	
-	});
-	return [yous, ais];
-}
-
-function attDef(arr) {
+function attDef(arr) {  	// 攻或守
 	var flag = 0;
 	var target = [];
 	winArr.forEach(function(item) {
@@ -201,17 +178,62 @@ function attDef(arr) {
 			}
 		});	
 	});
-	console.log(target)
 	return target;
 }
 
+function whoWin(who, arr){
+	var flag = 0;
+	winArr.forEach(function(item) {
+		flag = 0;
+		item.forEach(function(ite) {
+			arr.forEach(function(ite2) {
+				if (ite2.loc.toString() === ite.toString()) {
+					flag++;
+				}
+			});		 					
+			if (flag === 3) {
+				console.log("flag = "+ flag)
+				if (who === "ai") {
+					gameOver("You loss!");
+				} else {
+					gameOver("You win!");
+				}	
+				return flag;
+			}
+		});	
+	});	
+	return 0;
+}
 
+function scan(yous, ais){  // 扫描棋盘
+	var yous = [], ais = [];
+	Arr.forEach(function(item, index, array) {
+		if (item.val == you) {
+			yous.push(item);
+		} else if (item.val == ai) {
+			ais.push(item);
+		}	
+	});
+	return [yous, ais];
+}
+
+function winner(who) {
+	// 检测胜负
+	var yous = scan()[0];
+	var ais = scan()[1];
+	var a =  (who === "ai") ? ais : yous;
+	if(whoWin(who, a) > 0) {
+		return 3;
+	} else if (yous.length + ais.length === 9) {
+		return 9;
+	}
+}
 
 
 board.addEventListener("click", function(event) {
 	var target = event.target;
 	if (target.nodeName === "TD") {
-		if (!/fork|round/g.test(target.getAttribute("class"))) { //未落子
+		if (!/fork|round/g.test(target.getAttribute("class"))) {  // 未落子
 			for (var i = 0; i < Arr.length; i++) {
 				if (target == Arr[i].ele) {
 					Arr[i].val = you;
@@ -220,26 +242,44 @@ board.addEventListener("click", function(event) {
 			target.setAttribute("class", you);
 			step++;
 			console.log(step);
-				//检测胜负
-			yous = scan()[0];
-			ais = scan()[1];
-			whoWin("AI", ais);
-			if (yous.length + ais.length === 9) {
-				alert("平局");
-			}		
-
-			AI();  //轮到AI
+			AI();  // 轮到AI
 		}
 	}
 }, false);
 
 
 
+// 结束
+function gameOver(str) {
+	end.firstChild.innerHTML = str;
+	end.setAttribute("style", "display:block")	
+	pauseWrapper.setAttribute("style", "display:flex");  // 遮罩开
+}
 
+function clear() {
+	var round = document.getElementsByName('round')[0];
+	var fork = document.getElementsByName('fork')[0];
+	var first = document.getElementsByName('first')[0];	
+	fork.setAttribute("style", "background:white");
+	round.setAttribute("style", "background:white");
+	first.setAttribute("style", "background:white");
+	step = 0;
+	order = 0;
+	you = "";
+	ai = "";
+	Arr.forEach(function(item) {
+		item.val = null;
+		item.ele.setAttribute("class", "");
+	});
+	Arr = [];
+}
 
-
-//	结束
-
+restart.addEventListener("click", function(event) {
+	startWrapper.classList.remove("die");
+	clear();
+	end.setAttribute("style", "display:none");
+	pauseWrapper.setAttribute("style", "display:none");   // 遮罩关
+}, false);
 
 
 
